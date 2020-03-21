e4f9f5604c8c068be3caf788547442de0aae6580
dougall12 <s.dougall12@hotmail.co.uk>
2020-03-21 23:03:50 +0000

    Changes to be committed: 	modified:   app.js 	new file:   img/bee-logo.png 	new file:   img/placeholder.png 	new file:   img/sea.jpeg 	new file:   img/star.jpeg 	new file:   img/unit.jpg 	modified:   index.html 	modified:   style.css

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const open = document.getElementById('openNav');
-const close = document.getElementById('closeNav');
+const open = document.getElementById("openNav");
+const close = document.getElementById("closeNav");
 
 function openMenu() {
-  document.getElementById('small-nav').style.width = '50%';
-  document.getElementById('navbar').style.height = '0';
+  document.getElementById("small-nav").style.width = "50%";
+  document.getElementById("navbar").style.height = "0";
+  open.style.display = "none";
 }
 
 function closeMenu() {
-  document.getElementById('small-nav').style.width = '0';
-  document.getElementById('navbar').style.height = '50px';
+  document.getElementById("small-nav").style.width = "0";
+  document.getElementById("navbar").style.height = "50px";
+  open.style.display = "inline";
 }
 
-open.addEventListener('click', openMenu);
-close.addEventListener('click', closeMenu);
+open.addEventListener("click", openMenu);
+close.addEventListener("click", closeMenu);
