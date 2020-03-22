5d6e2b1fc54efcae8a6447fa393f8124382271ff
dougall12 <s.dougall12@hotmail.co.uk>
2020-03-22 20:01:28 +0000

    Added card tab functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Top Nav Responsive
+
 const open = document.getElementById("openNav");
 const close = document.getElementById("closeNav");
 
@@ -15,3 +17,39 @@ function closeMenu() {
 
 open.addEventListener("click", openMenu);
 close.addEventListener("click", closeMenu);
+
+// Tab Functionality
+
+// Click and toggle
+
+let tabList = document.querySelectorAll(".vertical-tab");
+let sections = document.querySelectorAll(".cards");
+console.log(sections);
+let buttons = document.querySelector("ul.tabs");
+
+buttons.addEventListener("click", () => {
+  let tabClicked = event.target;
+
+  //Check target is list item
+  if (tabClicked.nodeName === "LI") {
+    //Loop the tabs and toggle active
+
+    tabList.forEach(tab => {
+      if (tab.classList.contains("active") == true) {
+        tab.classList.toggle("active");
+        tabClicked.classList.toggle("active");
+      }
+    });
+
+    //Update Active Cards
+
+    sections.forEach(card => {
+      if (card.classList.contains("active") == true) {
+        card.classList.toggle("active");
+      }
+    });
+    let activePanelId = tabClicked.getAttribute("href");
+    let activeCard = document.querySelector(activePanelId);
+    activeCard.classList.toggle("active");
+  }
+});
